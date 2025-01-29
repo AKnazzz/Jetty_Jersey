00066f4fc94bae98ba5c2909d5dad011a0d7ab97
Andrey Knyazev <AKAKnaz@yandex.ru>
2025-01-29 18:15:59 +0300

    29/01/2025 test_main

--- src/main/java/ru/hh/jetty/Main.java
+++ src/main/java/ru/hh/jetty/Main.java
@@ -1,9 +1,17 @@
 package ru.hh.jetty;
 
+import org.eclipse.jetty.server.Server;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
+        var server = new Server(8080);
+        try{
+            server.start();
+            server.join(); //  для того, чтобы основной поток (в котором выполняется метод main) "присоединился" к потоку, на котором работает сервер. Это означает, что основной поток будет ждать, пока сервер не завершит свою работу.
+        } finally {
+            server.destroy();
+        }
     }
 
 }
