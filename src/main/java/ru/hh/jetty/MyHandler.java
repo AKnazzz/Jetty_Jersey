25ba9ad0e7d721630efcb46aa450688c8250a23e
Andrey Knyazev <AKAKnaz@yandex.ru>
2025-01-30 18:00:40 +0300

    30/01/2025 test_main

--- src/main/java/ru/hh/jetty/MyHandler.java
+++ src/main/java/ru/hh/jetty/MyHandler.java
@@ -4,12 +4,23 @@ import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Response;
 import org.eclipse.jetty.util.Callback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
 
 public class MyHandler extends Handler.Abstract.NonBlocking {
+    private static final Logger log = LoggerFactory.getLogger(MyHandler.class);
 
     @Override
     public boolean handle(Request request, Response response, Callback callback) throws Exception {
-        return false;
+        var path = request.getHttpURI().getPath();
+        if (path.contains("status")) {
+            log.info(" `status` in path");
+        }
+        var buffer = ByteBuffer.wrap("Hello from handler!".getBytes());
+        response.write(true, buffer, callback);
+        return true;
     }
 
 
